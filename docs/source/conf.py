8467d6c3a3dd71e438b7a5e9854e622ea29ffd4f
William F. Broderick <billbrod@gmail.com>
2025-01-03 15:09:32 -0500

    initial sphinx commit

---
+++ docs/source/conf.py
@@ -0,0 +1,71 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CCN software workshop, January 2025'
+copyright = '2025, Edoardo Balzani, Billy Broderick, Sarah Jo Venditto, Guillaume Viejo'
+author = 'Edoardo Balzani, Billy Broderick, Sarah Jo Venditto, Guillaume Viejo'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_nb',
+    'sphinx_copybutton',
+    'sphinx_togglebutton',
+]
+
+templates_path = []
+exclude_patterns = []
+
+nitpicky = True
+# raise an error if exec error in notebooks
+nb_execution_raise_on_error = True
+
+sphinxemoji_style = 'twemoji'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# max time (in secs) per notebook cell. here, we disable this
+nb_execution_timeout = -1
+# we have two versions of each notebook, one with explanatory text and one without
+# (which ends in `-stripped.md`). we don't need to run both of them
+nb_execution_excludepatterns = ['*stripped*']
+nb_execution_raise_on_error = True
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+html_favicon = '_static/ccn_small.png'
+html_sourcelink_suffix = ""
+myst_enable_extensions = ["colon_fence", "dollarmath", "attrs_inline"]
+html_theme_options = {
+    "home_page_in_toc": True,
+    "github_url": "https://github.com/flatironinstitute/ccn-software-jan-2025",
+    "repository_url": "https://github.com/flatironinstitute/ccn-software-jan-2025",
+    "logo": {
+        "alt_text": "Home",
+        "image_light": "_static/01-FI-primary-logo-color.png",
+        "image_dark": "_static/03-FI-primary-logo-white.png",
+    },
+    "use_download_button": True,
+    "use_repository_button": True,
+    "icon_links": [
+        {
+            "name": "Workshops home",
+            "url": "https://flatironinstitute.github.io/neurorse-workshops/",
+            "type": "fontawesome",
+            "icon": "fa-solid fa-house",
+        },
+        {
+            "name": "Binder",
+            "url": "https://binder.flatironinstitute.org/v2/user/wbroderick/cshl2024?filepath=introduction-stripped.ipynb",
+            "type": "url",
+            "icon": "https://mybinder.org/badge_logo.svg",
+        },
+    ],
+}
---
+++ docs/source/index.md
@@ -0,0 +1,12 @@
+# CCN Software Workshop, Jan 2025
+
+
+## Binder
+
+Some usage notes:
+
+- You are only allowed to have a single binder instance running at a time, so if you get the "already have an instance running error", go to the [binderhub page](https://binder.flatironinstitute.org/hub/hub/home) (or click on "check your currently running servers" on the right of the page) to join your running instance.
+- If you lose connection halfway through the workshop, go to the [binderhub page](https://binder.flatironinstitute.org/hub/hub/home) to join your running instance rather than restarting the image.
+- This is important because if you restart the image, **you will lose all data and progress**.
+- The binder will be shutdown automatically after 1 day of inactivity or 7 days of total usage. Data will not persist after the binder instance shuts down, so **please download any notebooks** you want to keep.
+- I will destroy this instance in 2 weeks. You can download your notebooks to keep them after the fact.
