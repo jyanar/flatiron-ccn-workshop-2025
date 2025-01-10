2858c5856338430d51d4ac9664ae652036e04444
William F. Broderick <billbrod@gmail.com>
2025-01-10 10:53:22 -0500

    adds env setting from Sarah Jo

--- src/workshop_utils/__init__.py
+++ src/workshop_utils/__init__.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
-from .fetch import DOWNLOADABLE_FILES
+from .fetch import DOWNLOADABLE_FILES, fetch_data
 from .plotting import *
+
+import os
+import pathlib
+
+repo_dir = pathlib.Path(__file__).parent.parent.parent
+os.environ["NEMOS_DATA_DIR"] = os.environ.get("NEMOS_DATA_DIR", str(repo_dir / "data"))
