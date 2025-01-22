e21d0e22e2b7e13c39a964defb9a5638ae054133
William F. Broderick <billbrod@gmail.com>
2025-01-22 14:59:05 -0500

    adds more info on click import error

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
-import click
+try:
+    import click
+except ImportError:
+    raise ImportError(
+        "We couldn't find click, which likely means we're not running from the right virtual "
+        "environment! Did you forget to activate your virtual env"
+        " (using `source` or `conda activate`, depending on how "
+        "you set it up)"
+    )
 import pathlib
 import shutil
 import subprocess
